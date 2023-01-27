eb5acd885e33d30bf1d2b18fbb17e38914579540
Faith Umunnakwe <faithuchinonso@gmail.com>
2023-01-27 02:33:03 +0100

    feature

---
+++ components/FeaturedRow.js
@@ -0,0 +1,61 @@
+import { View, Text, Platform, ScrollView } from "react-native";
+import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "./RestaurantCard";
+
+export default function FeaturedRow({ id, title, description }) {
+  return (
+    <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        {Platform.OS === "android" ? "" : <ArrowRightIcon color="#00CCBB" />}
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main str"
+          short_description="This is a test description"
+          dishes={[]}
+          long={1}
+          lat={1}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main str"
+          short_description="This is a test description"
+          dishes={[]}
+          long={1}
+          lat={1}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main str"
+          short_description="This is a test description"
+          dishes={[]}
+          long={1}
+          lat={1}
+        />
+      </ScrollView>
+    </View>
+  );
+}
---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'plx09jts',
+    dataset: 'production'
+  }
+})
